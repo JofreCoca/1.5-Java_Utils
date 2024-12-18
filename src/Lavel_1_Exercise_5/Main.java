997de5288e72d9fa9b558368b305069714dbda66
JofreCoca <jofrecoca@gmail.com>
2024-12-18 11:12:28 +0100

    Finish l1 e5

--- src/Lavel_1_Exercise_5/Main.java
+++ src/Lavel_1_Exercise_5/Main.java
@@ -8,18 +8,11 @@ public class Main {
         User user =new User("Pepe","1234");
         System.out.println(user);
         try {
-            ObjectSerializer.serializeObject(user,args[0]);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            User user2 = (User) ObjectSerializer.deserializeObject(args[0]);
+            ObjectSerializer.serializeObject(user,"/Users/jofrecocaavila/Documents/ITacademy/object.ser");//args[0]);
+            User user2 = (User) ObjectSerializer.deserializeObject("/Users/jofrecocaavila/Documents/ITacademy/object.ser");
             System.out.println(user2);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("catch objectSerializer");
         }
     }
 }
